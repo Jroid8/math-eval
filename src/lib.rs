4d1bebff4db29818f3448f3f7e69f7b65237bd24
Jamshid <jroid8@tutanota.com>
2024-05-16 19:39:04 +0330

    feat: add EvalBuilder

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::ops::RangeInclusive;
+use std::{collections::HashMap, ops::RangeInclusive};
 
 use asm::MathAssembly;
 use number::MathEvalNumber;
@@ -65,6 +65,266 @@ pub fn parse<'a, N: MathEvalNumber, V: Clone, F: Clone>(
         syntax_tree.0.root,
         function_to_pointer,
     ))
+}
+
+#[derive(Clone, Debug)]
+struct NoVariable;
+
+#[derive(Clone, Debug)]
+struct OneVariable(String);
+
+#[derive(Clone, Debug)]
+struct TwoVariables(String, String);
+
+#[derive(Clone, Debug)]
+struct ThreeVariables(String, String, String);
+
+#[derive(Clone, Debug)]
+struct FourVariables(String, String, String, String);
+
+#[derive(Clone, Debug)]
+struct ManyVariables(Vec<String>);
+
+#[derive(Clone)]
+pub struct EvalBuilder<'a, N, V>
+where
+    N: MathEvalNumber,
+{
+    constants: HashMap<String, N>,
+    function_identifier: HashMap<String, (usize, u8, Option<u8>)>,
+    functions: Vec<&'a dyn Fn(&[N]) -> N>,
+    variables: V,
+}
+
+impl<'a, N> Default for EvalBuilder<'a, N, NoVariable>
+where
+    N: MathEvalNumber,
+{
+    fn default() -> Self {
+        Self {
+            constants: HashMap::default(),
+            function_identifier: HashMap::default(),
+            functions: Vec::default(),
+            variables: NoVariable,
+        }
+    }
+}
+
+impl<'a, N, V> EvalBuilder<'a, N, V>
+where
+    N: MathEvalNumber,
+{
+    pub fn add_constant(mut self, name: impl Into<String>, value: N) -> Self {
+        self.constants.insert(name.into(), value);
+        self
+    }
+    pub fn add_function(
+        mut self,
+        name: impl Into<String>,
+        mininum_argument_count: u8,
+        maximum_argument_count: Option<u8>,
+        function: &'a dyn Fn(&[N]) -> N,
+    ) -> Self {
+        self.function_identifier.insert(
+            name.into(),
+            (
+                self.functions.len(),
+                mininum_argument_count,
+                maximum_argument_count,
+            ),
+        );
+        self.functions.push(function);
+        self
+    }
+}
+
+impl<'a, N> EvalBuilder<'a, N, NoVariable>
+where
+    N: MathEvalNumber,
+{
+    pub fn new() -> Self {
+        Self::default()
+    }
+    pub fn add_variable(self, name: impl Into<String>) -> EvalBuilder<'a, N, OneVariable> {
+        EvalBuilder {
+            constants: self.constants,
+            function_identifier: self.function_identifier,
+            functions: self.functions,
+            variables: OneVariable(name.into()),
+        }
+    }
+    pub fn build(self, input: &str) -> Result<impl Fn() -> N + 'a, ParsingError> {
+        let expr = parse(
+            input,
+            |inp| self.constants.get(inp).copied(),
+            |inp| self.function_identifier.get(inp).copied(),
+            |_| None,
+            |index| self.functions[*index],
+        )?;
+        Ok(move || expr.eval(|_: &()| 0.0.into()))
+    }
+}
+
+impl<'a, N> EvalBuilder<'a, N, OneVariable>
+where
+    N: MathEvalNumber,
+{
+    pub fn add_variable(self, name: impl Into<String>) -> EvalBuilder<'a, N, TwoVariables> {
+        EvalBuilder {
+            constants: self.constants,
+            function_identifier: self.function_identifier,
+            functions: self.functions,
+            variables: TwoVariables(self.variables.0, name.into()),
+        }
+    }
+    pub fn build(self, input: &str) -> Result<impl Fn(N) -> N + 'a, ParsingError> {
+        let expr = parse(
+            input,
+            |inp| self.constants.get(inp).copied(),
+            |inp| self.function_identifier.get(inp).copied(),
+            |inp| {
+                if self.variables.0 == inp {
+                    Some(())
+                } else {
+                    None
+                }
+            },
+            |index| self.functions[*index],
+        )?;
+        Ok(move |var| expr.eval(|_: &()| var))
+    }
+}
+
+impl<'a, N> EvalBuilder<'a, N, TwoVariables>
+where
+    N: MathEvalNumber,
+{
+    pub fn add_variable(self, name: impl Into<String>) -> EvalBuilder<'a, N, ThreeVariables> {
+        EvalBuilder {
+            constants: self.constants,
+            function_identifier: self.function_identifier,
+            functions: self.functions,
+            variables: ThreeVariables(self.variables.0, self.variables.1, name.into()),
+        }
+    }
+    pub fn build(self, input: &str) -> Result<impl Fn(N, N) -> N + 'a, ParsingError> {
+        let expr = parse(
+            input,
+            |inp| self.constants.get(inp).copied(),
+            |inp| self.function_identifier.get(inp).copied(),
+            |inp| {
+                [&self.variables.0, &self.variables.1]
+                    .into_iter()
+                    .position(|var| *var == inp)
+                    .map(|i| i == 0)
+            },
+            |index| self.functions[*index],
+        )?;
+        Ok(move |v1, v2| expr.eval(|var: &bool| if *var { v1 } else { v2 }))
+    }
+}
+
+impl<'a, N> EvalBuilder<'a, N, ThreeVariables>
+where
+    N: MathEvalNumber,
+{
+    pub fn add_variable(self, name: impl Into<String>) -> EvalBuilder<'a, N, FourVariables> {
+        EvalBuilder {
+            constants: self.constants,
+            function_identifier: self.function_identifier,
+            functions: self.functions,
+            variables: FourVariables(
+                self.variables.0,
+                self.variables.1,
+                self.variables.2,
+                name.into(),
+            ),
+        }
+    }
+    pub fn build(self, input: &str) -> Result<impl Fn(N, N, N) -> N + 'a, ParsingError> {
+        let expr = parse(
+            input,
+            |inp| self.constants.get(inp).copied(),
+            |inp| self.function_identifier.get(inp).copied(),
+            |inp| {
+                [&self.variables.0, &self.variables.1, &self.variables.2]
+                    .into_iter()
+                    .position(|var| *var == inp)
+            },
+            |index| self.functions[*index],
+        )?;
+        Ok(move |v1, v2, v3| {
+            expr.eval(|var: &usize| match var {
+                0 => v1,
+                1 => v2,
+                3 => v3,
+                _ => unreachable!(),
+            })
+        })
+    }
+}
+
+impl<'a, N> EvalBuilder<'a, N, FourVariables>
+where
+    N: MathEvalNumber,
+{
+    pub fn add_variable(self, name: impl Into<String>) -> EvalBuilder<'a, N, ManyVariables> {
+        EvalBuilder {
+            constants: self.constants,
+            function_identifier: self.function_identifier,
+            functions: self.functions,
+            variables: ManyVariables(vec![
+                self.variables.0,
+                self.variables.1,
+                self.variables.2,
+                self.variables.3,
+                name.into(),
+            ]),
+        }
+    }
+    pub fn build(self, input: &str) -> Result<impl Fn(N, N, N, N) -> N + 'a, ParsingError> {
+        let expr = parse(
+            input,
+            |inp| self.constants.get(inp).copied(),
+            |inp| self.function_identifier.get(inp).copied(),
+            |inp| {
+                [
+                    &self.variables.0,
+                    &self.variables.1,
+                    &self.variables.2,
+                    &self.variables.3,
+                ]
+                .into_iter()
+                .position(|var| *var == inp)
+            },
+            |index| self.functions[*index],
+        )?;
+        Ok(move |v1, v2, v3, v4| {
+            expr.eval(|var: &usize| match var {
+                0 => v1,
+                1 => v2,
+                3 => v3,
+                4 => v4,
+                _ => unreachable!(),
+            })
+        })
+    }
+}
+
+impl<'a, N> EvalBuilder<'a, N, ManyVariables>
+where
+    N: MathEvalNumber,
+{
+    pub fn build(self, input: &str) -> Result<impl Fn(&[N]) -> N + 'a, ParsingError> {
+        let expr = parse(
+            input,
+            |inp| self.constants.get(inp).copied(),
+            |inp| self.function_identifier.get(inp).copied(),
+            |inp| self.variables.0.iter().position(|var| *var == inp),
+            |index| self.functions[*index],
+        )?;
+        Ok(move |vars: &[N]| expr.eval(|v: &usize| vars[*v]))
+    }
 }
 
 #[cfg(test)]
@@ -147,10 +407,10 @@ pub(crate) fn test_func_to_pointer(func: &TestFunc) -> &'static dyn Fn(&[f64]) -
         inputs[0] * inputs[1] / gcd(inputs)
     }
     fn mean(inputs: &[f64]) -> f64 {
-        inputs.iter().sum::<f64>()/inputs.len() as f64
+        inputs.iter().sum::<f64>() / inputs.len() as f64
     }
     fn dist(inputs: &[f64]) -> f64 {
-        (inputs[0]*inputs[0] + inputs[1]*inputs[1]).sqrt()
+        (inputs[0] * inputs[0] + inputs[1] * inputs[1]).sqrt()
     }
 
     match func {
